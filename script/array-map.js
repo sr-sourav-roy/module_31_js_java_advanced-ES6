e7afc5c6caca427676f0e5bedf557f285e04eea6
sr-sourav-roy <sourav.cp.bd@gmail.com>
2026-03-09 13:26:24 +0600

    add my map and nested object

---
+++ script/array-map.js
@@ -0,0 +1,29 @@
+const number = [1, 2, 3, 4, 5, 6, 7, 9];
+
+// 👉mailtiplay:
+const newMalti = number.map((element) => {
+  return element * element;
+});
+// console.log(newMalti);
+
+// 👉other again
+const friendName = ["roy", "rocky", "function", "alom"];
+const newList = friendName.map((value, index) => {
+  return (value, index);
+});
+console.log(nameList);
+
+// 👉const newNumber = number.map((index) => index + 1);
+// 👉const newNumber = number.map((index) => {
+//   return index;
+// });
+// console.log(newNumber);
+
+// 👉basic type fo: loop -ing
+// let tem = [];
+// for (let index = 0; index < number.length; index++) {
+//   const element = number[index];
+//   const sum = element + 1;
+//   tem.push(sum);
+// }
+// console.log(tem);
